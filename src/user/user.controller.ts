f9ff262ec1f6c181c8d71a4595494a6537a31718
jamesgade <gadejames48@gmail.com>
2024-03-23 12:54:22 +0530

    Working nestjsx/crud app

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+// import { Crud, CrudController } from '@nestjsx/crud';
+import { Crud, CrudController } from '@dataui/crud';
+
+
+import { User } from './uder.entity';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
+@Crud({
+    model: {
+        type: User
+    }
+})
 @Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
+export class UserController implements CrudController<User>{
+    constructor(public service: UserService){
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { TypeOrmCrudService } from '@dataui/crud-typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './uder.entity';
 
 @Injectable()
-export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+export class UserService extends TypeOrmCrudService<User> {
+    constructor(@InjectRepository(User) userRepository){
+        super(userRepository);
+    }
 }
